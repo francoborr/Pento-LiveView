d30917bde872ab15eae2317c3829c9d39ec5547c
Franco Borrelli <franco@mimiquate.com>
2024-03-11 17:38:59 -0300

    update

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pento, Pento.Repo,
-  username: "postgres",
+  username: "franco",
   password: "postgres",
   hostname: "localhost",
   database: "pento_test#{System.get_env("MIX_TEST_PARTITION")}",
