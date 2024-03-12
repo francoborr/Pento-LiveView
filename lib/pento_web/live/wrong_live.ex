7cd8c7b0b6d0f5f48875f0624ab8333448d2148d
Franco Borrelli <franco@mimiquate.com>
2024-03-12 11:52:11 -0300

    chore: add validation

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,43 +1,58 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  alias Pento.Accounts
+
+  def mount(_params, session, socket) do
     {:ok,
      assign(socket,
        score: 0,
        message: "Make a guess:",
        time: DateTime.utc_now() |> to_string(),
        result: Enum.random(1..10),
-       has_won: false
+       has_won: false,
+       #  authentication security (we have this on the `on_mount` of the live sesion)
+       session_id: session["live_socket_id"]
      )}
-    |> IO.inspect()
   end
 
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
-    <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1> <h2>
-        <%= @message %>
-    It's <%= @time %>
-      </h2>
-    <br/>
+    <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
+
     <h2>
-    <%= if not @has_won do%>
+      <%= @message %> It's <%= @time %>
+    </h2>
+    <br />
+    <h2>
+      <%= if not @has_won do %>
         <%= for n <- 1..10 do %>
-          <.link class="bg-blue-500 hover:bg-blue-700
+          <.link
+            class="bg-blue-500 hover:bg-blue-700
           text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-          phx-click="guess" phx-value-number= {n} >
+            phx-click="guess"
+            phx-value-number={n}
+          >
             <%= n %>
           </.link>
         <% end %>
-        <% else %>
-        <.link class="bg-blue-500 hover:bg-blue-700
+      <% else %>
+        <.link
+          class="bg-blue-500 hover:bg-blue-700
         text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-        phx-click="reset">
-        Reset!
+          phx-click="reset"
+        >
+          Reset!
         </.link>
-     <% end %>
-     </h2>
+      <% end %>
+    </h2>
+
+    <br />
+    <pre>
+    <%= @current_user.email %>
+    <%= @session_id %>
+    </pre>
     """
   end
 
@@ -76,5 +91,9 @@ defmodule PentoWeb.WrongLive do
   end
 
   defp handle_guess_number(guess, _, socket),
-    do: %{message: "Your guess: #{guess}. Wrong. Guess again.", score: socket.assigns.score - 1, has_won: false}
+    do: %{
+      message: "Your guess: #{guess}. Wrong. Guess again.",
+      score: socket.assigns.score - 1,
+      has_won: false
+    }
 end
